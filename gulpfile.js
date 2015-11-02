428fd4b971e634a745e17cdefc27a64ceda00a2f
ryanwangtian <wtmanutd@163.com>
2015-11-02 15:15:15 +0800

    first commit, add env-left-menu and env-modal

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+var gulp = require('gulp'),
+	$ = require('gulp-load-plugins')()
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload;
+
+gulp.task('clean:dev', function() {
+	return gulp.src('.tmp')
+		.pipe($.clean({force: true}));
+});
+
+
+// gulp.task('copy', ['clean:dev'], function() {
+// 	return gulp.src('./src/**/*.js')
+//         .pipe(gulp.dest('.tmp'));
+// });
+
+gulp.task('sass:dev', function () {
+  return gulp.src(['./src/**/*.scss', '!./src/common/**/*.scss'])
+    .pipe($.sass({outputStyle: 'expanded'}).on('error', $.sass.logError))
+    .pipe(gulp.dest('./.tmp'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('js:dev', function () {
+	return gulp.src(['./src/**/*.js'])
+		.pipe(gulp.dest('.tmp'))
+		.pipe(reload({stream: true}));
+});
+
+
+gulp.task('serve', ['js:dev', 'sass:dev'], function() {
+	browserSync({
+        server: {
+            baseDir: ['demos', '.tmp'],
+            directory: true
+        }
+    });
+
+    gulp.watch("./src/**/*.scss", ['sass:dev']);
+    gulp.watch("./src/**/*.js", ['js:dev']);
+    gulp.watch("./demos/*.html", reload);
+});
+
+
+gulp.task('clean:dist', function() {
+    return gulp.src('./dist')
+        .pipe($.clean({force: true}));
+});
+gulp.task('js:dist', function () {
+    return gulp.src(['./src/**/*.js'])
+        .pipe(gulp.dest('./dist'))
+        .pipe($.uglify())
+        .pipe($.rename({
+                suffix: '.min'
+            }))
+        .pipe(gulp.dest('./dist'));
+});
+gulp.task('sass:dist', function () {
+  return gulp.src(['./src/**/*.scss', '!./src/common/**/*.scss'])
+    .pipe($.sass({outputStyle: 'expanded'}).on('error', $.sass.logError))
+    .pipe(gulp.dest('./dist'))
+    .pipe($.minifyCss())
+    .pipe($.rename({
+            suffix: '.min'
+        }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('bulid', ['clean:dist', 'js:dist', 'sass:dist'], function() {
+    $.runSequence('clean:dist', ['js:dist', 'sass:dist']);
+});
